a42a88abbb87a9805ef5b93b9928b6ced1566c77
Nan Tint Tint Tun <nantinttinttun.gic@gmail.com>
2021-12-25 15:05:32 +0630

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 	def sample_helper
-		"<p>My Helper</p>"
+		content_tag(:div, "My content", class: "my-class")
+	end
+
+	def source_helper(layout_name)
+		if session[:source]
+			greeting = "Thanks for visiting #{session[:source]} and you are on the #{layout_name} layout"
+      		content_tag(:p, greeting, class: "source-greeting")  
+		end
 	end
 end
